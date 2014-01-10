37ab7eb029e2cc4629b105a81fc9512e57fae20b
Keith Marshall <code@eleusis.co.uk>
2014-01-10 17:57:39 +0000

    Starting to neaten up the solution using an external transpose lib - still some way to go

--- prolog/day3/sudoku6.pl
+++ prolog/day3/sudoku6.pl
@@ -2,45 +2,49 @@
 % for 6x6 (3x2 rectangles).
 %
 % Naive solution here - just copy-paste and add more stuff (tedious!!)
+%
+% Gradually trying to make it shorter
 
 valid([]).
 valid([Head|Tail]) :- 
-    fd_all_different(Head), 
-    valid(Tail).
+	fd_domain(Head, 1, 6),
+    	fd_all_different(Head), 
+    	valid(Tail).
+
+% To make this more generic and more easily scaled to 9, I need to figure
+% out how to generate the column arrays from the rows.
+% I found the GNU-prolog 'nth()' clause, which might be of use here.
+% I've also found a transpose() function online which will do the job for me!
+
+:-include('transpose').
 
 sudoku6(Puzzle, Solution) :-
         Solution = Puzzle,
-        Puzzle = [S11, S12, S13, S14, S15, S16,
-                  S21, S22, S23, S24, S25, S26,
-                  S31, S32, S33, S34, S35, S36,
-                  S41, S42, S43, S44, S45, S46,
-                  S51, S52, S53, S54, S55, S56,
-                  S61, S62, S63, S64, S65, S66], 
-                  
-        fd_domain(Solution, 1, 6), 
+        Puzzle = [
+	        	[S11, S12, S13, S14, S15, S16],
+	        	[S21, S22, S23, S24, S25, S26],
+	        	[S31, S32, S33, S34, S35, S36],
+	        	[S41, S42, S43, S44, S45, S46],
+	        	[S51, S52, S53, S54, S55, S56],
+	        	[S61, S62, S63, S64, S65, S66]
+        	],
         
-        Row1 = [S11, S12, S13, S14, S15, S16],
-        Row2 = [S21, S22, S23, S24, S25, S26],
-        Row3 = [S31, S32, S33, S34, S35, S36],
-        Row4 = [S41, S42, S43, S44, S45, S46],
-        Row5 = [S51, S52, S53, S54, S55, S56],
-        Row6 = [S61, S62, S63, S64, S65, S66],
+        transpose(Puzzle, Columns),
         
-        Col1 = [S11, S21, S31, S41, S51, S61],
-        Col2 = [S12, S22, S32, S42, S52, S62],
-        Col3 = [S13, S23, S33, S43, S53, S63],
-        Col4 = [S14, S24, S34, S44, S54, S64],
-        Col5 = [S15, S25, S35, S45, S55, S65],
-        Col6 = [S16, S26, S36, S46, S56, S66],
+        Squares = [
+        		[S11, S12, S13, S21, S22, S23],
+        		[S14, S15, S16, S24, S25, S26],
+        		[S31, S32, S33, S41, S42, S43],
+        		[S34, S35, S36, S44, S45, S46], 
+        		[S51, S52, S53, S61, S62, S63],
+        		[S54, S55, S56, S64, S65, S66]
+        	],
         
-        Square1 = [S11, S12, S13, S21, S22, S23],
-        Square2 = [S14, S15, S16, S24, S25, S26],
-        Square3 = [S31, S32, S33, S41, S42, S43],
-        Square4 = [S34, S35, S36, S44, S45, S46], 
-        Square5 = [S51, S52, S53, S61, S62, S63],
-        Square6 = [S54, S55, S56, S64, S65, S66],
+        valid(Puzzle),
+        valid(Columns),
+        valid(Squares).
         
-        valid([Row1, Row2, Row3, Row4, Row5, Row6,
-               Col1, Col2, Col3, Col4, Col5, Col6,
-               Square1, Square2, Square3, Square4, Square5, Square6]).
-        
+% example:
+% sudoku6([[1,2,_,_,_,_], [6,_,_,_,_,2], [_,_,_,6,_,_],  [_,_,4,_,_,1], [_,_,_,2,_,3], [_,5,_,_,6,_]], What).
+% solution is:
+% What = [[1,2,5,4,3,6],[6,4,3,5,1,2],[2,3,1,6,4,5],[5,6,4,3,2,1],[4,1,6,2,5,3],[3,5,2,1,6,4]]
---
+++ prolog/day3/transpose.pl
@@ -0,0 +1,16 @@
+% Matrix transpose function, taken from here:
+% http://stackoverflow.com/questions/4280986/how-to-transpose-a-matrix-in-prolog
+
+transpose([], []).
+transpose([F|Fs], Ts) :-
+    transpose(F, [F|Fs], Ts).
+
+transpose([], _, []).
+transpose([_|Rs], Ms, [Ts|Tss]) :-
+        lists_firsts_rests(Ms, Ts, Ms1),
+        transpose(Rs, Ms1, Tss).
+
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+        lists_firsts_rests(Rest, Fs, Oss).
+        
