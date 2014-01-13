2582930eca6661261cf7bf3cf9aa17f872199c34
Keith Marshall <code@eleusis.co.uk>
2014-01-13 14:35:21 +0000

    Starting to make it more generic

--- prolog/day3/sudoku6.pl
+++ prolog/day3/sudoku6.pl
@@ -5,11 +5,13 @@
 %
 % Gradually trying to make it shorter
 
-valid([]).
-valid([Head|Tail]) :- 
-	fd_domain(Head, 1, 6),
+
+valid([], _).
+valid([Head|Tail], N) :- 
+	fd_domain(Head, 1, N),
     	fd_all_different(Head), 
-    	valid(Tail).
+    	valid(Tail, N).
+	
 
 % To make this more generic and more easily scaled to 9, I need to figure
 % out how to generate the column arrays from the rows.
@@ -19,6 +21,7 @@ valid([Head|Tail]) :-
 :-include('transpose').
 
 sudoku6(Puzzle, Solution) :-
+	length(Puzzle, N),
         Solution = Puzzle,
         Puzzle = [
 	        	[S11, S12, S13, S14, S15, S16],
@@ -27,7 +30,7 @@ sudoku6(Puzzle, Solution) :-
 	        	[S41, S42, S43, S44, S45, S46],
 	        	[S51, S52, S53, S54, S55, S56],
 	        	[S61, S62, S63, S64, S65, S66]
-        	],
+       	],
         
         transpose(Puzzle, Columns),
         
@@ -40,9 +43,9 @@ sudoku6(Puzzle, Solution) :-
         		[S54, S55, S56, S64, S65, S66]
         	],
         
-        valid(Puzzle),
-        valid(Columns),
-        valid(Squares).
+        valid(Puzzle, N),
+        valid(Columns, N),
+	valid(Squares, N).
         
 % example:
 % sudoku6([[1,2,_,_,_,_], [6,_,_,_,_,2], [_,_,_,6,_,_],  [_,_,4,_,_,1], [_,_,_,2,_,3], [_,5,_,_,6,_]], What).
