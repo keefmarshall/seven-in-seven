803336b28e4b22e0cf3aefa69db2ccb1142a98f6
Keith Marshall <keith.marshall@gmail.com>
2014-12-11 18:42:19 +0000

    Horrible method for finding rows.. eww.

--- erlang/day2/day2p3.erl
+++ erlang/day2/day2p3.erl
@@ -5,12 +5,38 @@
 
 -module(day2p3).
 -on_load(run/0).
+-define(BOARDSIZE, 3).
 
-board() -> [x, o, x,
-			o, x, o,
-			x, x, o].
+testBoard() -> [x, o, x,
+				o, x, o,
+				x, x, o].
 
+% Perhaps if I get better at foldLeft I can avoid writing all
+% these tail-recursive accumulation functions. Not sure.
+% This is currently quite insane, in my opinion. And I wrote it.
+rowFinder(PartialBoard, Acc) -> 
+	case PartialBoard of
+		[] -> Acc;
+		[X | Remainder] ->
+			case Acc of
+				[] -> rowFinder(Remainder, [[X]]);
+				[CurRow | Tail] ->
+					if 
+						length(CurRow) >= ?BOARDSIZE -> 
+							rowFinder(Remainder, [ [X] | Acc ]);
+						length(CurRow) < ?BOARDSIZE -> 
+							rowFinder(Remainder, [ [X | CurRow] | Tail ])
+					end
+			end
+	end.
+
+% rowFinder currently returns everything reversed. so, reversing the input
+% Board gives us everything back in the right order.
+rows(Board) -> rowFinder(lists:reverse(Board), []).
+			
 run() -> 
-	io:write(board()),
+	io:write(testBoard()),
+	io:fwrite("\n"),
+	io:write(rows(testBoard())),
 	io:fwrite("\n").
 	
